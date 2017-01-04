7b6368ad7628ca04eebb8e941ba367376cc200dc
Arseniy Bushyn <arseniybushyn@gmail.com>
2017-01-04 19:16:22 +0200

    add code highlighting & parallax & webgl

---
+++ highlight.js
@@ -0,0 +1,34 @@
+function highlight() {
+    var code = document.getElementsByTagName('code');
+    for (var i = 0; i < code.length; i++) {
+        code[i].innerHTML = code[i].innerHTML
+            .replace(/([(){}→∀λ,=]+|::=|:=)/g,
+                '<span class="h__symbol">$1</span>')
+            .replace(/\b(data|record|extend|let|define|case|receive|spawn|raise|send)\b(?!:)/g,
+                '<span class="h__keyword">$1</span>');
+    }
+}
+
+highlight();
+
+
+
+var lastScrollPosition = 0;
+var ticking = false;
+window.addEventListener('scroll', function(e) {
+    lastScrollPosition = window.scrollY;
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+        doSomething(lastScrollPosition);
+        ticking = false;
+        });
+    }
+    ticking = true;
+});
+
+var titles = document.querySelector('.header__titles');
+var logo = document.querySelector('.header__logo');
+function doSomething(scrollPos) {
+    titles.style.transform = 'translate3d(0px, ' + (scrollPos/2) + 'px, 0px)';
+    logo.style.transform = 'translate3d(0px, ' + (scrollPos/2) + 'px, 0px)';
+}
