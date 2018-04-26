7b63cfa910e99b1113ca603db8620645b906802f
Namdak Tonpa <maxim@synrc.com>
2018-04-26 17:13:26 +0300

    hi.js

--- highlight.js
+++ highlight.js
@@ -4,7 +4,7 @@ function highlight() {
         code[i].innerHTML = code[i].innerHTML
             .replace(/([(){}→∀λ,=]+|::=|:=)/g,
                 '<span class="h__symbol">$1</span>')
-            .replace(/\b(data|where|module|import|split|let|in|\|\=|\:|U)\b(?!:)/g,
+            .replace(/\b(data|where|module|import|split|let|in|\||=|:|U)\b(?!:)/g,
                 '<span class="h__keyword">$1</span>');
     }
 }
