f756be263d8cc03ff54eda3235a7d7480b80eacf
Namdak Tonpa <maxim@synrc.com>
2022-01-31 05:49:26 +0200

    inductive mltt foundations

--- highlight.js
+++ highlight.js
@@ -2,9 +2,9 @@ function highlight() {
     var code = document.getElementsByTagName('code');
     for (var i = 0; i < code.length; i++) {
         code[i].innerHTML = code[i].innerHTML
-            .replace(/([(){}→∀ΠΣλ,=]+|:|:=)/g,
+            .replace(/([(){}→=]+|:|:=)/g,
                 '<span class="h__symbol">$1</span>')
-            .replace(/\b(data|transp|glue|unglue|Glue|Anders|prover|MLTT|PTS|CCHM|HTS|deRham|hcomp|where|def|mutual|begin|end|module|import|option|false|true|.1|.2|Σ|Π|Pi|Sigma|W|𝟎|𝟏|𝟐|ind₂|ind₁|ind₀|★|0₂|1₂|Path|PathP|Type|Prop|inductive|record|forall|fun|match|let|axiom|theorem|in|U|S|V)\b(?!:)/g,
+            .replace(/\b(data|transp|∀|Π|Σ|λ|glue|unglue|Glue|Anders|prover|MLTT|PTS|CCHM|HTS|deRham|hcomp|where|def|mutual|begin|end|module|import|option|false|true|indᵂ|sup|.1|.2|Σ|Π|Pi|Sigma|W|𝟎|𝟏|𝟐|ind₂|ind₁|ind₀|★|0₂|1₂|Path|PathP|Type|Prop|inductive|record|forall|fun|match|let|axiom|theorem|lemma|in|U|S|V)\b(?!:)/g,
                 '<span class="h__keyword">$1</span>');
     }
 }
