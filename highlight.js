e28c71488ef248db1a25377ececb2e64d53e26ac
Namdak Tonpa <maxim@synrc.com>
2022-01-31 05:34:43 +0200

    inductive mltt foundations

--- highlight.js
+++ highlight.js
@@ -2,30 +2,17 @@ function highlight() {
     var code = document.getElementsByTagName('code');
     for (var i = 0; i < code.length; i++) {
         code[i].innerHTML = code[i].innerHTML
-            .replace(/([(){}→∀λ,=]+|:|:=)/g,
+            .replace(/([(){}→∀ΠΣλ,=]+|:|:=)/g,
                 '<span class="h__symbol">$1</span>')
-            .replace(/\b(data|transp|fill|hfill|transpFill|glue|unglue|Glue|Anders|prover|MLTT|PTS|CCHM|HTS|hcomp|where|def|mutual|begin|end|module|import|option|false|true|.1|.2|Pi|Sigma|Path|Type|Prop|inductive|record|forall|fun|match|let|axiom|theorem|in|U|S|V)\b(?!:)/g,
+            .replace(/\b(data|transp|glue|unglue|Glue|Anders|prover|MLTT|PTS|CCHM|HTS|deRham|hcomp|where|def|mutual|begin|end|module|import|option|false|true|.1|.2|Σ|Π|Pi|Sigma|W|𝟎|𝟏|𝟐|ind₂|ind₁|ind₀|★|0₂|1₂|Path|PathP|Type|Prop|inductive|record|forall|fun|match|let|axiom|theorem|in|U|S|V)\b(?!:)/g,
                 '<span class="h__keyword">$1</span>');
     }
 }
 
 highlight();
 
-
-
 var lastScrollPosition = 0;
 var ticking = false;
-
-// window.addEventListener('scroll', function(e) {
-//    lastScrollPosition = window.scrollY;
-//    if (!ticking) {
-//        window.requestAnimationFrame(function() {
-//        doSomething(lastScrollPosition);
-//        ticking = false;
-//        });
-//    }
-//    ticking = true;
-// });
 
 var titles = document.querySelector('.header__titles');
 var logo = document.querySelector('.header__logo');
